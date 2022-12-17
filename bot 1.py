fe407d7079c21086abf3c8359c818c6a6351e2e0
benice <73033349+SahilRana101@users.noreply.github.com>
2022-12-18 04:15:16 +0530

    Version 2 completed

--- bot 1.py
+++ bot 1.py
@@ -16,19 +16,18 @@ browser.get('https://www.instagram.com/')
 sleeper()
 username_input = browser.find_element(By.CSS_SELECTOR, "input[name='username']")
 password_input = browser.find_element(By.CSS_SELECTOR, "input[name='password']")
-sleeper()
 username_input.send_keys("macleodganjtrip@gmail.com")
 password_input.send_keys("selenium@1")
 login_button = browser.find_element(By.XPATH, "//button[@type='submit']")
 login_button.click()
 sleeper()
 
-print(browser.window_handles)
 
 # Opening files
 profile_links_file = open(r"profile links.txt", "r")
-user_following_file = open(r"user's following list.txt", "r+")
+user_following_file = open(r"users' following list.txt", "r+")
 done_with = open(r"done with profile links.txt", "r+")
+
 
 # making list of links from profile links file
 profile_links_list = []
@@ -38,6 +37,7 @@ for i in profile_links_file:
     else:
         profile_links_list.append(i)
 
+
 # making done with profile links list
 done_with_list = []
 for i in done_with:
@@ -45,6 +45,7 @@ for i in done_with:
         done_with_list.append(i[:-1])
     else:
         done_with_list.append(i)
+
 
 # making done with user's following list
 done_with_user_following_list = []
@@ -54,13 +55,12 @@ for i in user_following_file:
     else:
         done_with_user_following_list.append(i)
 
+
 # Iterating links and getting their following
 for i in profile_links_list:
     if i not in done_with_list:
         browser.get(i)
         sleeper()
-        # following_button = browser.find_element(By.CLASS_NAME, "_aacl")
-        # sleeper()
         count = browser.find_elements(By.CLASS_NAME, "_ac2a")[2].text
         count = count.replace(",", "")
         print(count)
@@ -74,6 +74,7 @@ for i in profile_links_list:
         # Getting their following
         while count_1 <= int(count):
             following = browser.find_elements(By.CLASS_NAME, "_ab8y")
+            print(count_1)
             if count_1 == prev_count:
                 count_2 += 1
             if count_2 == 4:
@@ -83,7 +84,6 @@ for i in profile_links_list:
             for j in range(count_1, len(following)):
                 browser.execute_script("arguments[0].scrollIntoView();", following[j])
                 count_1 += 1
-                print(following[j].text)
                 following_list.append(following[j].text)
             prev_count = count_1
             sleeper()
@@ -92,10 +92,13 @@ for i in profile_links_list:
         done_with.write(i + "\n")
 
         # Adding the following to final file of all following
-        print(following_list)
         for j in following_list:
             if j not in done_with_user_following_list:
                 user_following_file.write(j)
                 user_following_file.write("\n")
+
+profile_links_file.close()
+user_following_file.close()
+done_with.close()
 
 browser.close()
--- bot2.py
+++ bot2.py
@@ -17,10 +17,8 @@ sleeper()
 # Logging into Instagram
 browser.get('https://www.instagram.com/')
 sleeper()
-sleeper()
 username_input = browser.find_element(By.CSS_SELECTOR, "input[name='username']")
 password_input = browser.find_element(By.CSS_SELECTOR, "input[name='password']")
-sleeper()
 username_input.send_keys("macleodganjtrip@gmail.com")
 password_input.send_keys("selenium@1")
 login_button = browser.find_element(By.XPATH, "//button[@type='submit']")
@@ -28,13 +26,12 @@ login_button.click()
 sleeper()
 
 
-
 # Opening files
-user_following_file = open(r"user's following list.txt", "r")
-done_with = open(r"done with user's following list.txt", "r+")
-extra_file = open(r"Extra but important file.txt", "r")
+user_following_file = open(r"users' following list.txt", "r")
+done_with = open(r"done with users' following list.txt", "r+")
+extra_file = open(r"Extra file.txt", "r")
 
-# Shenanigans
+# Excel file number set
 count = 0
 for i in extra_file:
     count = i
@@ -45,7 +42,7 @@ sheet = book.add_worksheet()
 
 # closing and reopening extra file to write to it
 extra_file.close()
-extra_file = open(r"Extra but important file.txt", "w")
+extra_file = open(r"Extra file.txt", "w")
 extra_file.write(str(int(count)+1))
 
 # making user's following list
@@ -65,12 +62,12 @@ for i in done_with:
         done_with_user_following_list.append(i)
 
 
-# Shenanigans
+# row and column set
 row = 0
 column = 0
 
 
-# Iterating following list of users
+# iterating following list
 for i in user_following_list:
     if i not in done_with_user_following_list:
         browser.get("https://www.instagram.com/" + i + "/")
@@ -87,12 +84,15 @@ for i in user_following_list:
         count_1 = 0
         count_2 = 0
 
+        # iterate bio data tokens and find links and mentions
         for j in range(len(bio_data_tokens)):
             if "@" in bio_data_tokens[j] and bio_data_tokens[j].index("@") == 0:
                 mentions += str(bio_data_tokens[j])
                 mentions += ", "
                 count_1 = 1
-            if "." and "/" in bio_data_tokens[j]:
+            if "." and "/" in bio_data_tokens[j] or bio_data_tokens[j].endswith(".com") or bio_data_tokens[j].\
+                    endswith(".in") or bio_data_tokens[j].endswith(".org") or bio_data_tokens[j].endswith(".net") or \
+                    bio_data_tokens[j].endswith(".info") or bio_data_tokens[j].endswith(".biz"):
                 links += str(bio_data_tokens[j])
                 links += ", "
                 count_2 = 1
@@ -105,7 +105,12 @@ for i in user_following_list:
         row += 1
 
 
+# writing done with usernames to done with FILE
 for i in done_with_user_following_list:
     done_with.write(i + "\n")
+
+user_following_file.close()
+done_with.close()
+extra_file.close()
 
 book.close()
